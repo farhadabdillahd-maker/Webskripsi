1730d8c327044fb3081500371cc719c0079a8e6d
farhadabdillahd-maker <farhadabdillahd@gmail.com>
2026-07-08 02:29:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1280,7 +1280,6 @@ if menu == "Prediksi" and uploaded_file is None:
     st.markdown("""
     <div class="card">
     <h2>🔍 Prediksi Tingkat Kejahatan</h2>
-    <p>Prediksi dapat digunakan tanpa upload dataset.</p>
     </div>
     """, unsafe_allow_html=True)
 
