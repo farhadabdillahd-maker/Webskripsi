20527e756b0bafe144365fae5889c0fd3c42533b
farhadabdillahd-maker <farhadabdillahd@gmail.com>
2026-07-22 06:00:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2321,7 +2321,7 @@ if menu in ["Upload Dataset","Preprocessing","Klasifikasi"]:
         X_train, X_test, y_train, y_test = train_test_split(
             X_tfidf,
             y,
-            test_size=0.2,
+            test_size=0.5,
             random_state=42
         )
 
