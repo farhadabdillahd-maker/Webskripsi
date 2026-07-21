0aa71ad8a13a8fa472bb4dae6a7707bc0a3c7b60
farhadabdillahd-maker <farhadabdillahd@gmail.com>
2026-07-22 02:26:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,8 +169,8 @@ st.markdown("""
 <style>
 div[data-testid="stMarkdownContainer"] h1,
 div[data-testid="stMarkdownContainer"] h1 *{
-    color:#FFFFFF !important;
-    -webkit-text-fill-color:#FFFFFF !important;
+    color:#000000 !important;
+    -webkit-text-fill-color:#000000 !important;
     opacity:1 !important;
 }
 </style>
@@ -185,8 +185,8 @@ div[data-testid="stMarkdownContainer"] h1 *{
 st.markdown("""
 <style>
 div[data-testid="stMarkdownContainer"] h1{
-    color:#FFFFFF !important;
-    -webkit-text-fill-color:#FFFFFF !important;
+    color:#000000 !important;
+    -webkit-text-fill-color:#000000 !important;
     opacity:1 !important;
 }
 </style>
@@ -463,7 +463,7 @@ def show_home():
         <div style='font-size:15px;letter-spacing:2px;color:#bfdbfe'>
         POLRES PASAMAN • MACHINE LEARNING • NAÏVE BAYES
         </div>
-        <h1 style='font-size:42px;font-weight:900;line-height:1.35;margin-top:22px;text-shadow:0 3px 12px rgba(0,0,0,.55);color:#FFFFFF !important;-webkit-text-fill-color:#FFFFFF !important;opacity:1 !important;color:#FFFFFF !important;-webkit-text-fill-color:#FFFFFF !important;'>
+        <h1 style='font-size:42px;font-weight:900;line-height:1.35;margin-top:22px;text-shadow:0 3px 12px rgba(0,0,0,.55);color:#000000 !important;-webkit-text-fill-color:#000000 !important;opacity:1 !important;color:#000000 !important;-webkit-text-fill-color:#000000 !important;'>
         PENERAPAN MACHINE LEARNING MENGGUNAKAN ALGORITMA NAÏVE BAYES
         <br>UNTUK KLASIFIKASI TINGKAT KEJAHATAN
         </h1>
