e1d0672de63bffebd83a6d27d6c4fb40756c78b9
farhadabdillahd-maker <farhadabdillahd@gmail.com>
2026-07-22 06:13:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2322,7 +2322,7 @@ if menu in ["Upload Dataset","Preprocessing","Klasifikasi"]:
             X_tfidf,
             y,
             test_size=0.5,
-            random_state=123
+            random_state=7
         )
 
         # =====================================
